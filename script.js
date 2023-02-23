581ce6f1068ad36c83804ba4551f708d5ee52549
Isabel Sopha <i.sopha94@gmail.com>
2023-02-23 13:30:46 -0800

    changed js to functions

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
-let userName = prompt("What is your name?");
-console.log(userName);
-let userChoice = prompt("Do you like legos " + userName + "?");
-console.log(userChoice);
-
-if (userName == "") {
-    userName = "Stranger";
+function someQuestions() {
+    let userName = prompt("What is your name?");
+    console.log(userName);
+    if (userName == "") {
+        userName = alert("❗❌❗Stranger Danger❗❌❗");
+        userName = prompt("Try Again. What is your name?");
+        userName = "Stranger";
+    }
+    document.write("Hello " + userName + "! \r\n");
+    return userName;
 };
 
-if (userChoice == "yes" || userChoice == "yea" || userChoice == "sure") {
-    greeting = "You're in for a treat!";
-}   else if (userChoice == "no" || userChoice == "nah" || userChoice == "nope") {
-    greeting = "Well here is my personal collections page anyway!";
-}   else {
-    greeting = "I'm not sure what you mean.";
+
+function greeting() {
+    let userName = someQuestions();
+    let userChoice = prompt("Do you like legos " + userName + "?");
+    let personalGreet = "";
+    console.log(userChoice);
+    if (userChoice == "yes" || userChoice == "yea" || userChoice == "sure") {
+        personalGreet = document.write("You're in for a treat!");
+        console.log("Hello");
+    } else if (userChoice == "no" || userChoice == "nah" || userChoice == "nope") {
+        personalGreet = document.write("Well here is my personal collections page anyway!");
+    } else {
+        personalGreet = document.write("I'm not sure what you mean but everyone likes legos.");
+    }
+    document.write(" Thanks for visiting my page, " + userName + "! Have a good day.");
+    return personalGreet;
 };
 
-// document.getElementById('lego')
-// document.write("hello " + userName + greeting)
 
-let lego2 = document.getElementById("lego");
-console.log(lego2);
-lego2.textContent = "Hello " + userName + "! \r\n" ;
-lego2.textContent += greeting;
 
-let legoElm = document.getElementById("legos2");
-console.log(legoElm);
-legoElm.textContent = "This is my personal lego collections page. Enjoy!";
 
-let lego3 = document.getElementById("legos2");
-console.log(lego3);
-legoElm.textContent = "Thanks for visiting my page, " + userName  + "! Have a good day.";
+
+
+
+// function goodbye() {
+//     let goodbyeMsg = someQuestions();
+//     let goodbyeMsg = document.write(" Thanks for visiting my page, " + userName + "! Have a good day.");
+// };
+
+// greeting();
+
+
+// someQuestions();
+
+// let legoElm = document.getElementById("legos2");
+// console.log(legoElm);
+// legoElm.textContent = "This is my personal lego collections page. Enjoy!";
+
+// let lego3 = document.getElementById("legos2");
+// console.log(lego3);
+// legoElm.textContent = "Thanks for visiting my page, " + userName  + "! Have a good day.";
